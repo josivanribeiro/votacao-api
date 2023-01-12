64030e10821d06f50af3d2ce9f2621c5436edf80
Josivan Ribeiro da Silva <josivan@gmail.com>
2023-01-12 17:28:00 -0300

    Refactoring api tests

--- src/test/java/com/votacaoapi/controller/VotacaoControllerTest.java
+++ src/test/java/com/votacaoapi/controller/VotacaoControllerTest.java
@@ -1,109 +1,142 @@
-//package com.votacaoapi.controller;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.votacaoapi.Application;
-//import com.votacaoapi.dto.VotacaoDTO;
-//import com.votacaoapi.service.PautaService;
-//import org.hamcrest.core.Is;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mockito;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.times;
-//import static org.mockito.Mockito.verify;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = Application.class)
-//@AutoConfigureMockMvc
-//public class VotacaoControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private PautaService pautaService;
-//
-//    private static final ObjectMapper om = new ObjectMapper();
-//
-//    @Test
-//    public void shouldSavePautaSuccessfully() throws Exception {
-//        Mockito.when(pautaService.save(any(VotacaoDTO.class))).thenReturn(getRespostaVotacaoDTOMock());
-//
-//        mockMvc.perform(post("/votacoes/")
-//                        .content(om.writeValueAsString(getNewVotacaoDTOMock()))
-//                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
-//                .andDo(print())
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.botaoOk.body.campo1", Is.is("Sim")))
-//                .andExpect(jsonPath("$.botaoOk.body.campo2", Is.is(69151332086L)));
-//
-//        verify(pautaService, times(1)).save(any(VotacaoDTO.class));
-//    }
-//
-//    @Test
-//    public void shouldReturnInternalServerErrorWhenSavePauta() throws Exception {
-//        Mockito.when(pautaService.save(any(VotacaoDTO.class))).thenThrow(IllegalArgumentException.class);
-//
-//        mockMvc.perform(post("/votacoes/")
-//                        .content(om.writeValueAsString(getNewVotacaoDTOMock()))
-//                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
-//                .andDo(print())
-//                .andExpect(status().isInternalServerError());
-//
-//        verify(pautaService, times(1)).save(any(VotacaoDTO.class));
-//    }
-//
-////    @Test
-////    public void getMembershipsByRoleOK() throws Exception {
-////        List<MembershipDTO> membershipDTOs = new ArrayList<>();
-////        MembershipDTO membershipDTO = new MembershipDTO();
-////        membershipDTO.setUserId("10");
-////        membershipDTO.setTeamId("50");
-////        membershipDTOs.add(membershipDTO);
-////
-////        Mockito.when(roleService.getMembershipsByRole(ROLE_ID)).thenReturn(membershipDTOs);
-////
-////        mockMvc.perform(get("/roles/getmembershipsbyrole?roleId=1")
-////                        .content(om.writeValueAsString(membershipDTOs))
-////                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
-////                .andExpect(status().isOk())
-////                .andExpect(jsonPath("$.[0].userId", Is.is("10")))
-////                .andExpect(jsonPath("$.[0].teamId", Is.is("50")));
-////
-////        verify(roleService, times(1)).getMembershipsByRole(ROLE_ID);
-////    }
-//
-//    private VotacaoDTO getNewVotacaoDTOMock() {
-//        VotacaoDTO votacaoDTO = new VotacaoDTO();
-//        votacaoDTO.setCampo1("Sim");
-//        votacaoDTO.setCampo2(69151332086L);
-//        votacaoDTO.setIdCampoTexto("Texto");
-//        votacaoDTO.setIdCampoNumerico("999");
-//        votacaoDTO.setIdCampoData("01/01/2000");
-//        return votacaoDTO;
-//    }
-//
-//    private RespostaVotacaoDTO getRespostaVotacaoDTOMock() {
-//        RespostaVotacaoDTO newRespostaVotacaoDTO = new RespostaVotacaoDTO();
-//        BotaoOKDTO botaoOKDTO = new BotaoOKDTO();
-//        BodyDTO bodyDTO = new BodyDTO();
-//        bodyDTO.setCampo1("Sim");
-//        bodyDTO.setCampo2(69151332086L);
-//        botaoOKDTO.setBody(bodyDTO);
-//        newRespostaVotacaoDTO.setBotaoOk(botaoOKDTO);
-//        return newRespostaVotacaoDTO;
-//    }
-//}
+package com.votacaoapi.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.votacaoapi.Application;
+import com.votacaoapi.dto.*;
+import com.votacaoapi.exception.EntityNotFoundException;
+import com.votacaoapi.service.PautaService;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+public class VotacaoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PautaService pautaService;
+
+    private static final ObjectMapper om = new ObjectMapper();
+
+    @Test
+    public void shouldSavePautaSuccessfully() throws Exception {
+        Mockito.when(pautaService.save(any(VotacaoDTO.class))).thenReturn(getPautaDTOMock());
+
+        mockMvc.perform(post("/votacoes/")
+                        .content(om.writeValueAsString(getNewVotacaoDTOMock()))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", Is.is("1")))
+                .andExpect(jsonPath("$.nome", Is.is("Opção 1")));
+
+        verify(pautaService, times(1)).save(any(VotacaoDTO.class));
+    }
+
+    @Test
+    public void shouldReturnInternalServerErrorWhenSavePauta() throws Exception {
+        Mockito.when(pautaService.save(any(VotacaoDTO.class))).thenThrow(IllegalArgumentException.class);
+
+        mockMvc.perform(post("/votacoes/")
+                        .content(om.writeValueAsString(getNewVotacaoDTOMock()))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isInternalServerError());
+
+        verify(pautaService, times(1)).save(any(VotacaoDTO.class));
+    }
+
+    @Test
+    public void shouldSaveSelectionSuccessfully() throws Exception {
+        Mockito.when(pautaService.saveSelection(any(SelecaoDTO.class))).thenReturn(getFullPautaDTOMock());
+
+        mockMvc.perform(post("/votacoes/selecao")
+                        .content(om.writeValueAsString(getNewSelecaoDTOMock()))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", Is.is("1")))
+                .andExpect(jsonPath("$.nome", Is.is("Opção 1")))
+                .andExpect(jsonPath("$.votos[0].associado.cpf", Is.is("34871040089")))
+                .andExpect(jsonPath("$.votos[0].descricao", Is.is("Sim")));
+
+        verify(pautaService, times(1)).saveSelection(any(SelecaoDTO.class));
+    }
+
+    @Test
+    public void shouldReturnNotFoundErrorWhenSaveSelection() throws Exception {
+        Mockito.when(pautaService.saveSelection(any(SelecaoDTO.class))).thenThrow(EntityNotFoundException.class);
+
+        mockMvc.perform(post("/votacoes/selecao")
+                        .content(om.writeValueAsString(getNewSelecaoDTOMock()))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+        verify(pautaService, times(1)).saveSelection(any(SelecaoDTO.class));
+    }
+
+    private VotacaoDTO getNewVotacaoDTOMock() {
+        VotacaoDTO votacaoDTO = new VotacaoDTO();
+        votacaoDTO.setCampo1("Sim");
+        votacaoDTO.setCampo2(69151332086L);
+        votacaoDTO.setIdCampoTexto("Texto");
+        votacaoDTO.setIdCampoNumerico("999");
+        votacaoDTO.setIdCampoData("01/01/2000");
+        return votacaoDTO;
+    }
+
+    private SelecaoDTO getNewSelecaoDTOMock() {
+        SelecaoDTO selecaoDTO = new SelecaoDTO();
+        selecaoDTO.setCampo1("1");
+        selecaoDTO.setCampo2(69151332086L);
+        selecaoDTO.setCampo3("Sim");
+        return selecaoDTO;
+    }
+
+    private PautaDTO getPautaDTOMock() {
+        PautaDTO pautaDTO = new PautaDTO();
+        pautaDTO.setNome("Opção 1");
+        pautaDTO.setId("1");
+        return pautaDTO;
+    }
+
+    private PautaDTO getFullPautaDTOMock() {
+        PautaDTO pautaDTO = new PautaDTO();
+        pautaDTO.setNome("Opção 1");
+        pautaDTO.setId("1");
+        VotoDTO votoDTO = new VotoDTO();
+        AssociadoDTO associadoDTO = new AssociadoDTO();
+        associadoDTO.setCpf("34871040089");
+        votoDTO.setAssociado(associadoDTO);
+        votoDTO.setDescricao("Sim");
+        List<VotoDTO> votoDTOs = new ArrayList<>();
+        votoDTOs.add(votoDTO);
+        pautaDTO.setVotos(votoDTOs);
+        return pautaDTO;
+    }
+}
